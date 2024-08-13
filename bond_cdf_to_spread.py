2bc8af584e063c522669bf62865f5c75bace7d56
Charles Rambo <76271967+charlesrambo@users.noreply.github.com>
2024-08-13 08:24:08 -0700

    Simplified calculation
    Don't need to think of everything in terms of what happens in interval (t_{k - 1}, t_k].

--- bond_cdf_to_spread.py
+++ bond_cdf_to_spread.py
@@ -12,64 +12,56 @@ from scipy.optimize import fsolve
 def get_bond_price_from_cdf(coupon, tenor, recovery_rate, riskfree_rate, cdf):
     
     # Get times of coupon payments
-    coupon_times = np.arange(tenor, 0, -0.5)
+    t_coupons = np.arange(tenor, 0, -0.5)
     
-    # Partiton interval [0, T] uniformly 
-    time_partition = np.linspace(0, tenor, 2 * len(coupon_times) + 1)
+    # Partiton interval [0, tenor] uniformly 
+    t_partition = np.linspace(0, tenor, 2 * len(t_coupons) + 1)
     
-    # Initialize bond price
-    bond_price = 0
+    # Add in coupons in cases where there's no default before time t
+    bond = coupon/2 * np.sum([(1 - cdf(t)) * np.exp(-riskfree_rate * t) 
+                                          for t in t_coupons])    
+        
+    # Add principal payment in cases where there's no default during the life of bond
+    bond += 100 * np.exp(-riskfree_rate * tenor) * (1 - cdf(tenor))
     
-    # Loop over subintervals
-    for t0, t1 in zip(time_partition[:-1], time_partition[1:]):
-        
-        # Cash flow if default times probability of default in [t0, t1]
-        bond_price += recovery_rate * np.exp(-t1 * riskfree_rate) * (cdf(t1) - cdf(t0))
-        
-        # Get payments in interval
-        payment_times = coupon_times[(t0 < coupon_times) & (coupon_times <= t1)]
-        
-        # PV of payments times probabilities of making payments. Sum results
-        bond_price += coupon/2 * np.sum([(1 - cdf(t)) * np.exp(-riskfree_rate * t) 
-                                         for t in payment_times])
-        
-    # Add principal payment times probability of payment
-    bond_price += 100 * np.exp(-riskfree_rate * tenor) * (1 - cdf(tenor))
+    # Add in value in cases where there's a default
+    bond += recovery_rate * np.sum([np.exp(-riskfree_rate * t1) * (cdf(t1) - cdf(t0)) 
+                        for t0, t1 in zip(t_partition[:-1], t_partition[1:])])
     
-    return bond_price
+    return bond
 
 
 def get_bond_price_from_spread(coupon, tenor, recovery_rate, riskfree_rate, spread):
     
     # Get times of coupon payments
-    coupon_times = np.arange(tenor, 0, -0.5)
+    t_coupons = np.arange(tenor, 0, -0.5)
 
     # Get cash flows
-    cash_flows = np.repeat(coupon/2, len(coupon_times)) 
+    cash_flows = np.repeat(coupon/2, len(t_coupons)) 
     
     # Add principal payment to final cash flow
     cash_flows[0] += 100
     
     # Calculate discount factors
-    discount_factors = np.exp(-(riskfree_rate + spread) * coupon_times)
+    discount_factors = np.exp(-(riskfree_rate + spread) * t_coupons)
     
     # Get PV
-    bond_price = np.sum(cash_flows * discount_factors)
+    bond = np.sum(cash_flows * discount_factors)
     
-    return bond_price
+    return bond
 
 
 def get_spread(coupon, tenor, recovery_rate, riskfree_rate, cdf, spread0 = 0.05):
     
     # Get bond price using cdf
-    bond_price = get_bond_price_from_cdf(coupon, tenor, recovery_rate, 
+    bond = get_bond_price_from_cdf(coupon, tenor, recovery_rate, 
                                              riskfree_rate, cdf)
     
     try:
         
         # Get objective function
-        obj = lambda spread: bond_price - get_bond_price_from_spread(coupon, 
-                                tenor, recovery_rate, riskfree_rate, spread)
+        obj = lambda spread: bond - get_bond_price_from_spread(coupon, tenor, 
+                                        recovery_rate, riskfree_rate, spread)
         
         # Solve for spread
         spread = 100**2 * fsolve(obj, x0 = spread0)[0]
@@ -80,14 +72,13 @@ def get_spread(coupon, tenor, recovery_rate, riskfree_rate, cdf, spread0 = 0.05)
         
         return np.nan
     
-        
-# =============================================================================
+# ============================================================================= 
 # tenor = 10
 # coupon = 5
 # recovery_rate = 40
 # riskfree_rate = 0.04
 # 
-# cdf = lambda t: 1 - np.exp(-0.10 * t)
+# cdf = lambda t: 1 - np.exp(-0.03 * t)
 # =============================================================================
         
     
